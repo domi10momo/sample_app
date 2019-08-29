8e748d6855e6a98b0db56faca9c14cbdcc6bd981
domi10momo <hb.kmaia93@icloud.com>
2019-08-29 19:16:55 +0900

    flash機能追加 #7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      #redirect_to user_url(@user)と同義
+      redirect_to @user
     else
       render 'new'
     end
