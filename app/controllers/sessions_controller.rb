53e28ef7954ad0c4d5fc39c51abbbde31383a4b5
domi10momo <hb.kmaia93@icloud.com>
2019-08-31 07:35:27 +0900

    login session作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #ユーザーログイン後にユーザ情報のページにリダイレクト
+    else
+      #エラーメッセージを作成
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
